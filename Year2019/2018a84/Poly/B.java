358318b12bd87ce6c8836d7dc83520e0c63257cd
MorielHarush <93482738+MorielHarush@users.noreply.github.com>
2023-02-03 16:05:44 +0200

    Create B.java

---
+++ Year2019/2018a84/Poly/B.java
@@ -0,0 +1,21 @@
+package Poly3;
+
+public class B extends A{
+
+		public B() {
+			System.out.println("B");
+		}
+		
+		public void arik() {
+			System.out.println("Arik_B");
+		}
+		
+		public void yosef() {
+			System.out.println("Yosef");
+		}
+		
+		public void superYosef() {
+			super.yosef();
+		
+	}
+}
