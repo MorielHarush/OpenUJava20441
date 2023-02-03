3e944dd4384a72e4745e29c249b6b068f0ffd45c
MorielHarush <93482738+MorielHarush@users.noreply.github.com>
2023-02-03 16:05:34 +0200

    Create A.java

---
+++ Year2019/2018a84/Poly/A.java
@@ -0,0 +1,19 @@
+package Poly3;
+
+public class A {
+
+	public A() {
+		System.out.println("A");
+	}
+	
+	public void arik() {
+		System.out.println("Arik_A");
+	}
+	
+	public void yosef() {
+		arik();
+	}
+	
+	
+}
+
