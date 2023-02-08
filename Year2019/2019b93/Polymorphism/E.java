103680c68833486706e3f65ae01785180d41b989
MorielHarush <93482738+MorielHarush@users.noreply.github.com>
2023-02-08 19:03:10 +0200

    Create E.java

---
+++ Year2019/2019b93/Polymorphism/E.java
@@ -0,0 +1,12 @@
+package Poly2019b93;
+
+public class E extends C{
+
+	public void bbb() {
+		aaa();
+		System.out.println("E - bbb()");
+		}
+		public void ccc() {
+		System.out.println("E - ccc()");
+		}
+}
