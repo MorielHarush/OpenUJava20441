56c692ed212cdc3e7b47b61b5af3ee486768a2ac
MorielHarush <93482738+MorielHarush@users.noreply.github.com>
2023-02-08 19:02:57 +0200

    Create C.java

---
+++ Year2019/2019b93/Polymorphism/C.java
@@ -0,0 +1,12 @@
+package Poly2019b93;
+
+public class C extends A{
+
+	public void bbb() {
+		System.out.println("C - bbb()");
+		super.ccc();
+		}
+		public void ccc() {
+		System.out.println("C - ccc()");
+		}
+}
