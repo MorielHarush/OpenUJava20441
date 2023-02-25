8ecda5bf632fbc4b0e550e0ccce7d88fcfed4597
MorielHarush <93482738+MorielHarush@users.noreply.github.com>
2023-02-25 14:21:50 +0200

    Create Driver.java

---
+++ Year2021/August2021b92/LinkedLists/Driver.java
@@ -0,0 +1,30 @@
+package Lists2021b92;
+
+public class Driver {
+
+	public static void main(String[] args) {
+	    IntNode node5 = new IntNode(5,null);
+	    IntNode node4 = new IntNode(4, node5);
+	    IntNode node3 = new IntNode(3, node4);
+	    IntNode node2 = new IntNode(2, node3);
+	    IntNode node1 = new IntNode(1, node2);
+	    
+	    // Create a list
+	    IntNode.IntList testList = node1.new IntList();
+	    
+//	    IntNode node10 = new IntNode(5,null);
+	    IntNode node9 = new IntNode(3, null);
+	    IntNode node8 = new IntNode(4, node9);
+	    IntNode node7 = new IntNode(2, node8);
+	    IntNode node6 = new IntNode(1, node7);
+	    
+	    // Create Second List
+	    IntNode.IntList list = node6.new IntList();
+	    
+	    // Check
+	    boolean result = testList.what1(list);
+	    System.out.println("First Result:" + result);
+	    boolean result2 = testList.what2(list);
+	    System.out.println("Second Result:" + result2);
+	}
+}
